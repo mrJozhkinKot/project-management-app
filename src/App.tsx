861a0d463d9a6a7d37f93aa7b7a00600d2e70251
Yulia Shchuklina <scrambler88@mail.ru>
2022-05-02 15:17:27 +0300

    feat: create react app

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FunctionComponent = () => {
+  return <div className="App">Project-Managment-App</div>;
+};
+
+export default App;
