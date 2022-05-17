f7957435e9cd74abf96443ffd2f31698d91a0bcb
Yulia Shchuklina <scrambler88@mail.ru>
2022-05-17 22:58:16 +0300

    fix: delete unnecessary files

--- src/components/pages/Board.tsx
+++ src/components/pages/Board.tsx
@@ -6,23 +6,13 @@ import ColumnList from '../boards/ColumnList';
 import ModalColumn from '../modal/ModalColumn';
 import ModalEditTask from '../modal/ModalEditTask';
 import ModalTask from '../modal/ModalTask';
-import { getColumnsThunk } from '../../reducers/ActionBoardsCreater';
-import { useParams } from 'react-router-dom';
 import ConfirmColumnModal from '../modal/ConfirmColumnModal';
 import ConfirmTaskModal from '../modal/ConfirmTaskModa';
 
 const Board: React.FC = () => {
   const { setIsModalColumn } = boardsSlice.actions;
   const dispatch = useAppDispatch();
-  const { id } = useParams();
   const { currentColumnId, currentTaskId } = useAppSelector((state) => state.boardsReducer);
-
-  useEffect(() => {
-    if (id) {
-      dispatch(getColumnsThunk(id));
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   const style = {
     margin: '2rem',
